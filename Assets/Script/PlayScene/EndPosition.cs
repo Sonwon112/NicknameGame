9c6f91d07e9221c2d8001cc1273abf602817a6d7
Sonwonill <tlatmsdkqk123@gmail.com>
2024-11-14 00:52:54 +0900

    :sparkles: Feat : "애니메이션 작업 완료 및 달리기 기본기능 구현 완료"
    애니메이션 작업 완료 및 달리기 기본기능 구현 완료

---
+++ Assets/Script/PlayScene/EndPosition.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndPosition : MonoBehaviour
+{
+    public GameManager GameManager;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+
+        }   
+    }
+}
