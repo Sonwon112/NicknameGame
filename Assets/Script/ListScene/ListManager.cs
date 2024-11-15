c9199535c445325978bb9df6bade860efa98ee4d
Sonwonill <tlatmsdkqk123@gmail.com>
2024-11-16 02:10:21 +0900

    :sparkles: Feat : "맵 선택 후 참여자 모집 기능 구현 중"

--- Assets/Script/ListScene/ListManager.cs
+++ Assets/Script/ListScene/ListManager.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class ListManager : MonoBehaviour
 {
+    public GameObject ParticipantWindow;
+    
+    private ListContent currListContent;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,8 +20,15 @@ public class ListManager : MonoBehaviour
         
     }
 
-    public void OpenContent()
+    public void OpenContent(string value)
     {
+        GameObject gameObject = GameObject.Find(value);
+        if(gameObject == null)
+        {
+            Debug.Log("해당 이름의 컨텐츠 목록이 존재하지 않습니다");
+            return;
+        }
+        currListContent = gameObject.GetComponent<ListContent>();
 
     }
 
---
+++ Assets/Script/ListScene/ParticipantWindow.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticipantWindow : MonoBehaviour
+{
+    public Sprite Thumbnail { get; set; }
+    public GameObject ParticipantContent;
+    public GameObject ScrollView;
+
+    private int currPosY;
+    private int interval = 40;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
